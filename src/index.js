931b6bb9c90a0d6f394bac5faba1367e1be4a683
Arpit <arpitdalal21@gmail.com>
2020-06-01 13:54:46 -0300

    basic express server setup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const port = process.env.PORT
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+app.listen(port, () => {
+    console.log(`Server initialized on port ${port}`)
+})
